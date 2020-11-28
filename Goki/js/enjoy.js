4b6e6102decd35b89692015247dd714f1b5ca0d4
20taxitaxi00 <takushi0210@icloud.com>
2020-11-29 01:41:36 +0900

    home完了

--- Goki/js/enjoy.js
+++ Goki/js/enjoy.js
@@ -1,23 +1,23 @@
 $(function(){
+
+
     // タブメニュー
     $('.tab-nav a').on('click', function(){
         // activeがあった時の処理
         if ($(this).hasClass('active')) {
             return false;
         }
+
         // activeがなかった時の処理
         $('.tab-nav a').removeClass('active');
         $(this).addClass('active');
-        $('.tab-content > div').removeClass('active');
-        $('.tab-content > div').filter(this.hash).addClass('active');
+        $('.tab-content > section').removeClass('active');
+        $('.tab-content > section').filter(this.hash).addClass('active');
         return false;
 
+        
+
     });
-
-
-
-
-
 
 
 });
