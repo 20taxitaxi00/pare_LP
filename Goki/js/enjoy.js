a7a8143aa80a0acb3c794edcea9664b43b3bc1d1
20taxitaxi00 <takushi0210@icloud.com>
2020-11-30 16:52:51 +0900

    ハンバーガーメニュー完了

--- Goki/js/enjoy.js
+++ Goki/js/enjoy.js
@@ -21,5 +21,24 @@ $(function(){
     });
 
 
+    // ハンバーガーメニュー
+	$('#q7-btn').on('click',function(){
+		let isActive = $(this).hasClass('on');
+		toggleDrower(isActive);
+    });
+    
+    // ドロワー
+	function toggleDrower(isActive) {
+	    $('.js-hamburger')
+	    if (isActive) {
+	      $('#drower-bg').fadeOut(600);
+	    } else {
+	      $('#drower-bg').fadeIn(600);
+	    }
+	    $('.js-hamburger').toggleClass('on');
+	    $('.js-drower').toggleClass('on');
+	  }
+
+
 });
 
